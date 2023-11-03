01b51f1aeb0cbfbf40237a3973e2b6d546f24296
cinko95 <>
2023-11-03 01:03:24 +0000

    Implement game board display

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ board = [['O' for _ in range(6)] for _ in range(6)]
 import random
 
 def place_ship(board):
-    ship_row = random.randint(0, 4)
-    ship_col = random.randint(0, 4)
+    ship_row = random.randint(0, 5)
+    ship_col = random.randint(0, 5)
     board[ship_row][ship_col] = 'S'
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
