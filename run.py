c0bcc9bb8fbd36d4c2f28f5569d3596d31ba9fe7
cinko95 <>
2023-11-03 01:02:06 +0000

    Add code to place a ships

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-board = [['O' for _ in range(5)] for _ in range(5)]
+board = [['O' for _ in range(6)] for _ in range(6)]
+
+import random
+
+def place_ship(board):
+    ship_row = random.randint(0, 4)
+    ship_col = random.randint(0, 4)
+    board[ship_row][ship_col] = 'S'
