4853a2dfbe2d7b28c11090232d80725c90a95104
cinko95 <>
2023-11-03 01:00:53 +0000

    created board structure

--- run.py
+++ run.py
@@ -1,3 +1,1 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = [['O' for _ in range(5)] for _ in range(5)]
